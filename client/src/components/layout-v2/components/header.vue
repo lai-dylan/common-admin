fd61bd1537ecc8ca452c27bf391b0d3a7f4b8e41
dylan <dylan@test.com>
2026-02-26 00:18:08 +0800

    temp

--- client/src/components/layout-v2/components/header.vue
+++ client/src/components/layout-v2/components/header.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="header-container">header</div>
+  <header class="header-container">
+    <div class="header-title">IM聊天后台管理</div>
+
+    <div class="header-actions cursor-pointer text-xs">toggle</div>
+  </header>
 </template>
 
 <script setup lang="ts"></script>
@@ -7,7 +11,22 @@
 <style scoped lang="scss">
 .header-container {
   width: 100%;
-  height: 100px;
+  height: 70px;
   border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+  padding: 20px 28px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.header-title {
+  color: #fff;
+  font-size: 16px;
+  line-height: 22px;
+}
+
+.header-actions {
+  display: flex;
+  align-items: flex-end;
 }
 </style>
